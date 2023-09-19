7562ab2c2a6024e3d6672e4fee82fe5c7e4ef4d5
Pwikster <134342742+Pwikster@users.noreply.github.com>
2023-09-19 16:01:39 -0500

    Began development on accounts/user integration

--- loremaster_project/loremaster_app/views.py
+++ loremaster_project/loremaster_app/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render, HttpResponse
 
 # Runs from urls.py -> loremaster_app.urls.py then to here.
 def index(request, *args, **kwargs): #*args and ** kwargs are tuples that hold arguments that are passed to the view therefore the HTML
+
     return render(request, 'index.html')
